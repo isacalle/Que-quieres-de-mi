735bca462bb0e34816e3d5404025af6d9157e4a3
isacalle <isabelacalle.dev@gmail.com>
2024-02-18 20:58:44 -0500

    Cambio ciclos

--- js/main.js
+++ js/main.js
@@ -7,22 +7,34 @@ alert("Hola " + nombre + " bienvenidx a Qué quieres de mí!");
 saludar(prompt("ingrese su nombre"));
 
 
-//algoritmo condicional
+// Algoritmo de ciclo for
+
+for (let i=1; i <10; i++){
+    //en cada repeticion, solicitamos un nombre
+    
+    let correo = prompt("ingrese su correo");
+    alert(" Numero de compra "+ i + " Correo: " + correo); 
+    console.log("Numero de compra " + i + " correo: " + correo)
+    break;
+}
+
+
+//algoritmos condicionales
 
 let prenda1 = "Bralette";
 let prenda2 = "Body";
 let prenda3 = "Corsé";
 
 prendaEscogida = prompt("Elija una de las prendas");
-if (prendaEscogida == "Bralette") {
+if (prendaEscogida === "Bralette") {
     console.log("Bralette");
     alert("precio: " + 25);
 
-} else if (prendaEscogida == "Body") {
+} else if (prendaEscogida === "Body") {
     console.log("Body");
     alert("precio: " + 30);
 
-} else if (prendaEscogida == "Corsé") {
+} else if (prendaEscogida === "Corsé") {
     console.log("Corsé");
     alert("Precio: " + 35);
 } else {
@@ -30,8 +42,6 @@ if (prendaEscogida == "Bralette") {
     alert("Prenda no encontrada")
 }
 
-
-// Algoritmo de ciclo 
 
 let ingresarCantidad= parseInt( prompt("Ingresa cuantas prendas desea (maximo 3)"));
 
@@ -43,12 +53,12 @@ break;
 
 case 2:
 console.log("2 prendas");
-alert("dos prendas seleccionada");
+alert("dos prendas seleccionadas");
 break;
 
 case 3:
 console.log("3 prendas");
-alert("tres prendas seleccionada");
+alert("tres prendas seleccionadas");
 break;
 
 default: 
@@ -57,3 +67,8 @@ alert("cantidad no disponible");
 }
 
 alert("has escogido " + ingresarCantidad + " " + prendaEscogida);
+
+
+
+
+
