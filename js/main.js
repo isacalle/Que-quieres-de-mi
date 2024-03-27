31c8027d71f54545a10e00f3285af6a8f4784295
isacalle <isabelacalle.dev@gmail.com>
2024-03-27 04:00:05 -0500

    Tercer commmit

--- js/main.js
+++ js/main.js
@@ -1,112 +1,139 @@
-/*
-//funcion simple
-function saludar(nombre) {
-    console.log("nombre de usuario: " + nombre);
-    alert("Hola " + nombre + " bienvenidx a ¡Qué quieres de mí!");
+
+//Array de los productos 
+const productos = [
+    {
+        //bralettes
+        id: "prenda1",
+        titulo: "bralette negro",
+        imagen: "../img/Screenshot 2024-03-18 at 8.21.08 PM.png",
+        precio: 25,
+        categoria: {
+            color: "negro"
+        }
+    },
+    {
+        id: "prenda2",
+        titulo: "bralette rosa",
+        imagen: "../img/Screenshot 2024-03-18 at 8.17.05 PM.png",
+        precio: 25,
+        categoria: {
+            color: "rosa"
+        }
+    },
+    {
+        id: "prenda3",
+        titulo: "bralette verde",
+        imagen: "../img/332b07f1c49c06a78f4a18ec4d9de6d5.jpg",
+        precio: 25,
+        categoria: {
+            color: "verde"
+        }
+    },
+    {
+        id: "prenda4",
+        titulo: "bralette azul",
+        imagen: "../img/ea4ea4ed0dda078245350bb8f9a96156.jpg",
+        precio: 25,
+        categoria: {
+            color: "azul"
+        }
+    },
+    {
+        //bodys
+        id: "prenda5",
+        titulo: "body durazno",
+        imagen: "../img/Screenshot 2024-03-18 at 8.04.58 PM.png",
+        precio: 30,
+        categoria: {
+            color: "durazno"
+        }
+    },
+    {
+        id: "prenda6",
+        titulo: "body negro",
+        imagen: "../img/Screenshot 2024-03-18 at 8.09.18 PM.png",
+        precio: 30,
+        categoria: {
+            color: "negro"
+        }
+    },
+    {
+        id: "prenda7",
+        titulo: "body naranja",
+        imagen: "../img/7fc863f4db721375562b7d5a8be176b6.jpg",
+        precio: 30,
+        categoria: {
+            color: "naranja"
+        }
+    },
+    {
+        id: "prenda8",
+        titulo: "body café",
+        imagen: "../img/61820c2ea38e91d4d29c8489e3115698.jpg",
+        precio: 30,
+        categoria: {
+            color: "cafe"
+        }
+    },
+    {
+        //corses
+        id: "prenda9",
+        titulo: "corsé negro",
+        imagen: "../img/Screenshot 2024-03-18 at 8.04.00 PM.png",
+        precio: 35,
+        categoria: {
+            color: "negro"
+        }
+    },
+    {
+        id: "prenda10",
+        titulo: "corsé morado",
+        imagen: "../img/Screenshot 2024-03-18 at 8.03.06 PM.png",
+        precio: 35,
+        categoria: {
+            color: "morado"
+        }
+    },
+    {
+        id: "prenda11",
+        titulo: "corsé azul",
+        imagen: "../img/7613d7860a3d9a7f87eba893d8b82df7.jpg",
+        precio: 35,
+        categoria: {
+            color: "azul"
+        }
+    },
+    {
+        id: "prenda12",
+        titulo: "corsé rosa",
+        imagen: "../img/Screenshot 2024-03-18 at 8.00.24 PM.png",
+        precio: 35,
+        categoria: {
+            color: "rosa"
+        }
+    },
+];
+
+//funcion para mostrar los productos 
+const cajitaProductos = document.querySelector("#cajita__productos");
+
+
+function cargarProductos() {
+    productos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+    <img class="cajita__imagen" src="${producto.imagen}" alt="${producto.titulo}">
+    <h5 class="subtitulo__productos">${producto.titulo}</h5>
+    <p class="producto__precio">$${producto.precio}</p>
+    <button class="boton" id="${producto.id}" type="submit">Añadir a la compra</button>
+    `; 
+    cajitaProductos.append(div);
+
+    })
 }
-saludar(prompt("¡Hola! Ingresa tu nombre :)"));
 
+cargarProductos();
 
-//función constructora
-function Prenda(id, nombre, precio, talla) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.talla = talla;
-}
-
-//objetos 
-const prenda1 = new Prenda(1, "Bralette", 25, "S");
-const prenda2 = new Prenda(2, "Bralette", 25, "M");
-const prenda3 = new Prenda(3, "Bralette", 25, "L");
-const prenda4 = new Prenda(4, "Body", 30, "S");
-const prenda5 = new Prenda(5, "Body", 30, "M");
-const prenda6 = new Prenda(6, "Body", 30, "L");
-const prenda7 = new Prenda(7, "Corsé", 35, "S");
-const prenda8 = new Prenda(8, "Corsé", 35, "M");
-const prenda9 = new Prenda(9, "Corsé", 35, "L");
-
-
-//array y forEach para generar la lista de las prendas con sus descripciones 
-const listaPrendas = [prenda1, prenda2, prenda3, prenda4, prenda5, prenda6, prenda7, prenda8, prenda9];
-let mensaje = "Elija una de las prendas disponibles: \n";
-listaPrendas.forEach((prenda) => {
-    mensaje += `${prenda.id}: ${prenda.nombre} - $${prenda.precio} - Talla ${prenda.talla}\n`;
-});
-
-
-//switch
-let prendaEscogida = parseInt(prompt(mensaje));
-let prendaSeleccionada;
-switch (prendaEscogida) {
-    case 1:
-        prendaSeleccionada = prenda1;
-        console.log(prenda1);
-        alert("Has escogido un " + prenda1.nombre + " talla " + prenda1.talla + " a un valor de " + prenda1.precio + " dlrs");
-        break;
-
-    case 2:
-        prendaSeleccionada = prenda2;
-        console.log(prenda2);
-        alert("Has escogido un " + prenda2.nombre + " talla " + prenda2.talla + " a un valor de " + prenda2.precio + " dlrs");
-        break;
-
-    case 3:
-        prendaSeleccionada = prenda3;
-        console.log(prenda3);
-        alert("Has escogido un " + prenda3.nombre + " talla " + prenda3.talla + " a un valor de " + prenda3.precio + " dlrs");
-        break;
-
-    case 4:
-        prendaSeleccionada = prenda4;
-        console.log(prenda4);
-        alert("Has escogido un " + prenda4.nombre + " talla " + prenda4.talla + " a un valor de " + prenda4.precio + " dlrs");
-        break;
-
-    case 5:
-        prendaSeleccionada = prenda5;
-        console.log(prenda5);
-        alert("Has escogido un " + prenda5.nombre + " talla " + prenda5.talla + " a un valor de " + prenda5.precio + " dlrs");
-        break;
-
-    case 6:
-        prendaSeleccionada = prenda6;
-        console.log(prenda6);
-        alert("Has escogido un " + prenda6.nombre + " talla " + prenda6.talla + " a un valor de " + prenda6.precio + " dlrs");
-        break;
-
-    case 7:
-        prendaSeleccionada = prenda7;
-        console.log(prenda7);
-        alert("Has escogido un " + prenda7.nombre + " talla " + prenda7.talla + " a un valor de " + prenda7.precio + " dlrs");
-        break;
-
-    case 8:
-        prendaSeleccionada = prenda8;
-        console.log(prenda8);
-        alert("Has escogido un " + prenda8.nombre + " talla " + prenda8.talla + " a un valor de " + prenda8.precio + " dlrs");
-        break;
-
-    case 9:
-        prendaSeleccionada = prenda9;
-        console.log(prenda9);
-        alert("Has escogido un " + prenda9.nombre + " talla " + prenda9.talla + " a un valor de " + prenda9.precio + " dlrs");
-        break;
-    default:
-        console.log("prenda no disponible");
-        alert("prenda no disponible");
-}
-
-let cantidad = parseInt(prompt("Ingresa cuantas prendas deseas:"));
-console.log("Cantidad de prendas: " + cantidad);
-alert("Has seleccionado: " + cantidad + " " + prendaSeleccionada.nombre);
-
-
-
-//variable para sumar la prenda seleccionada con la cantidad deseada 
-let totalCompra = prendaSeleccionada.precio * cantidad;
-console.log("El total de la compra es: " + totalCompra + " dlrs");
-alert("El total de tu compra es de: " + totalCompra + " dlrs");
-
-
-*/
+    
